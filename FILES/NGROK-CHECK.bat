57c1d4e2b5ca0fa22a087c075b00ae8e4b75476e
ans20045 <86540575+ans20045@users.noreply.github.com>
2021-08-05 08:08:26 +0200

    Update NGROK-CHECK.bat

--- FILES/NGROK-CHECK.bat
+++ FILES/NGROK-CHECK.bat
@@ -1,6 +1,6 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+net config server /srvcomment:"Windows Server 2019 By 卐ヅANASヅ卍" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -O https://raw.githubusercontent.com/ans2004/RDP/main/FILES/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Telegram.exe" "https://raw.githubusercontent.com/ans2004/RDP/main/FILES/Telegram.exe" > out.txt 2>&1
